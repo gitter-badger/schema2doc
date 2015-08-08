6078aada1d8b5982deb8916f4ae907396f9561b7
man-at-home <man@home.com>
2015-08-09 01:21:22 +0200

    11: support one in memory database for development
    Task-Url: http://github.com/man-at-home/schema2doc/issues/issue/11

--- src/main/java/org/manathome/schema2doc/Schema2DocCmd.java
+++ src/main/java/org/manathome/schema2doc/Schema2DocCmd.java
@@ -1,5 +1,13 @@
 package org.manathome.schema2doc;
 
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.manathome.schema2doc.renderer.IRenderer;
 import org.manathome.schema2doc.renderer.impl.AsciidocRenderer;
 import org.manathome.schema2doc.scanner.IScanner;
@@ -7,6 +15,8 @@ import org.manathome.schema2doc.scanner.impl.MockScanner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.PrintWriter;
 
 /**
  * command line interface.
@@ -21,17 +31,66 @@ public class Schema2DocCmd {
    /**
     * command line entry point.
     *  
-	* @param args
+	* @param  args see -help command
+	* @see    Schema2DocCmd#printHelp()
     * @throws Exception 
 	*/
 	public static void main(String[] args) throws Exception {
 		System.out.println("processing oracle schema");
 		LOG.debug("running commandline");
-		final IScanner  scanner =  new MockScanner();					// TBD: fill with commandline
-		final IRenderer renderer = new AsciidocRenderer(System.out);	// TBD: fill with commandline
-		Schema2Doc s2d = new Schema2Doc(scanner, renderer);
-		s2d.process();
-		System.out.println("processed " + s2d.getRenderedTables() + " tables.");
+		
+		
+	    CommandLineParser parser = new DefaultParser();
+	    try {
+	        final CommandLine cmdLine = parser.parse(createCommandLineOptions(), args);
+	    
+	        if (cmdLine.hasOption("help")) {
+	        	printHelp(new PrintWriter(System.out));
+	        } else {
+				final IScanner  scanner =  new MockScanner();					// TBD: fill with commandline
+				final IRenderer renderer = new AsciidocRenderer(System.out);	// TBD: fill with commandline
+				Schema2Doc s2d = new Schema2Doc(scanner, renderer);
+				s2d.process();
+				System.out.println("processed " + s2d.getRenderedTables() + " tables.");
+	        }
+			
+	    } catch (ParseException exp) {
+	        System.err.println("Parsing failed.  Reason: " + exp.getMessage());
+	    }
+	}
+	
+	
+	
+	/** define command line options with commons-cli. */ 
+	public static Options createCommandLineOptions() {
+	
+		Options cmdOptions = new Options();
+		
+		cmdOptions.addOption(Option.builder("c").desc("jdbc connection string for the db to be documented.")
+				.argName("url").longOpt("connection").numberOfArgs(1).build());
+		cmdOptions.addOption(Option.builder("driver").desc("jdbc driver class (fqn) to use, default is ..")
+				.argName("className").numberOfArgs(1).build());
+		cmdOptions.addOption(Option.builder("o")
+				.desc("output directory for asciidoc output (default is stdout/console)")
+				.longOpt("out").build());
+		cmdOptions.addOption(Option.builder("h").desc("command line help").longOpt("help").build());
+		
+		return cmdOptions;
+	}
+	
+	/** print help (on -help command). */
+	public static void printHelp(PrintWriter pw) {
+		LOG.debug("printing help");
+		
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp(
+				pw, 80, "schema2doc", 
+				"\nschema2doc tool, retrieves database metadata from a db an writes asciidoc docs with it.\n\n", 
+				createCommandLineOptions(), 3, 5,
+				"\nasciidoc documenation generator for your database", true
+				);
+		
+		pw.flush();
 	}
 
 }
--- src/test/java/org/manathome/schema2doc/Schema2DocCmdTest.java
+++ src/test/java/org/manathome/schema2doc/Schema2DocCmdTest.java
@@ -1,7 +1,13 @@
 package org.manathome.schema2doc;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 /** tests. */
 public class Schema2DocCmdTest {
@@ -11,8 +17,25 @@ public class Schema2DocCmdTest {
 	}
 
 	@Test
-	public void test() throws Exception {
+	public void testSampleRun() throws Exception {
 		Schema2DocCmd.main(null);
+	}
+	
+	@Test
+	public void testHelp() throws Exception {
+		Schema2DocCmd.main(new String[] {"-help"});
+	}
+
+	@Test
+	public void testHelpContent() throws Exception {
+		StringWriter sw = new StringWriter();
+		Schema2DocCmd.printHelp(new PrintWriter(sw));
+		String helpContent = sw.toString();
+		
+		assertThat(helpContent, containsString("schema2doc"));
+		assertThat(helpContent, containsString("-help"));
+		assertThat(helpContent, containsString("-driver"));
+		assertThat(helpContent, containsString("-connection"));
 	}
 
 }
