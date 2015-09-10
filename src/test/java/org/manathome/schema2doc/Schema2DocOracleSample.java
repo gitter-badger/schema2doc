169dd8e3b643816e41a99a556af0e26df7b010c4
man-at-home <man@home.com>
2015-09-10 21:02:13 +0200

    fix oracle conditional test

--- src/test/java/org/manathome/schema2doc/Schema2DocOracleSample.java
+++ src/test/java/org/manathome/schema2doc/Schema2DocOracleSample.java
@@ -2,9 +2,7 @@ package org.manathome.schema2doc;
 
 import static org.junit.Assert.*;
 
-
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.manathome.schema2doc.renderer.IRenderer;
 import org.manathome.schema2doc.renderer.impl.AsciidocRenderer;
@@ -18,17 +16,17 @@ import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
-/** test with larger oracle db. 
+/**
+ * test with larger oracle db.
  * 
  * 
- * */
+ */
 public class Schema2DocOracleSample {
 
-	
 	private static final Logger LOG = LoggerFactory.getLogger(Schema2DocOracleSample.class);
-	
+
 	private IScanner scanner;
-	
+
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -36,27 +34,29 @@ public class Schema2DocOracleSample {
 	/** optional test: using oracle. */
 	@Test
 	public void testEoxe() throws Exception {
-		
-			this.scanner = ScannerFactory.getInstance().getScanner(
-					"Oracle",
-					"oracle.jdbc.driver.OracleDriver", 
-					"jdbc:oracle:thin:@ebseoxdpc01:1521:eoxe", 
-					System.getenv("EOXE_USER"), 
-					System.getenv("EOXE_PW"));
-					
-			this.scanner.setSchemaFilter(new String[] {"EOX"});
-			
-			File outFile = new File("src/docs/examples/schema2doc.eoxe.db.oracle.example.asciidoc");
-			LOG.debug("writing test asciidoc to " + outFile.getAbsolutePath());
-			
-			IRenderer renderer = new AsciidocRenderer(
-					new PrintWriter(new OutputStreamWriter(new FileOutputStream(outFile), "UTF-8"), true)
-					);
-			Schema2Doc s2d = new Schema2Doc(scanner, renderer);
-			s2d.process();
-			
-			assertTrue("asciidoc not exists", outFile.exists());
-			assertTrue("asciidoc not filled", outFile.length() > 10);
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException cnfe) {
+			LOG.info("omitting oracle test eoxe, no suitable driver.. " + cnfe.getMessage());
+			return;
+		}
+
+		this.scanner = ScannerFactory.getInstance().getScanner("Oracle", "oracle.jdbc.driver.OracleDriver",
+				"jdbc:oracle:thin:@ebseoxdpc01:1521:eoxe", System.getenv("EOXE_USER"), System.getenv("EOXE_PW"));
+
+		this.scanner.setSchemaFilter(new String[] { "EOX" });
+
+		File outFile = new File("src/docs/examples/schema2doc.eoxe.db.oracle.example.asciidoc");
+		LOG.debug("writing test asciidoc to " + outFile.getAbsolutePath());
+
+		IRenderer renderer = new AsciidocRenderer(
+				new PrintWriter(new OutputStreamWriter(new FileOutputStream(outFile), "UTF-8"), true));
+		Schema2Doc s2d = new Schema2Doc(scanner, renderer);
+		s2d.process();
+
+		assertTrue("asciidoc not exists", outFile.exists());
+		assertTrue("asciidoc not filled", outFile.length() > 10);
 	}
 
 }
